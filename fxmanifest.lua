8654c0610781a6a35f59d013089f9998e4de984f
KNFH <71471907+KNFH@users.noreply.github.com>
2021-02-22 22:09:08 +0200

    Create fxmanifest.lua
    Our version to the framework

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+version '0.0.1'
+
+client_scripts {
+    ----------------------------------------
+    -- Rage UI Thing
+    ----------------------------------------
+    'RageUI/src/RMenu.lua',
+    'RageUI/src/menu/RageUI.lua',
+    'RageUI/src/menu/Menu.lua',
+    'RageUI/src/menu/MenuController.lua',
+
+    'RageUI/src/components/*.lua',
+
+    'RageUI/src/menu/elements/*.lua',
+
+    'RageUI/src/menu/items/*.lua',
+
+    'RageUI/src/menu/panels/*.lua',
+
+    'RageUI/src/menu/panels/*.lua',
+    'RageUI/src/menu/windows/*.lua',
+
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
